e40b4c4d195eabe1a623484a519b6fd366f430cc
Alex Cannizzo <cansal89@gmail.com>
2021-10-06 22:59:36 +0700

    - Set up folders for view, models and view models

---
+++ City Sights/Views/LaunchView.swift
@@ -0,0 +1,20 @@
+//
+//  LaunchView.swift
+//  City Sights
+//
+//  Created by Alex Cannizzo on 06/10/2021.
+//
+
+import SwiftUI
+
+struct LaunchView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LaunchView_Previews: PreviewProvider {
+    static var previews: some View {
+        LaunchView()
+    }
+}
