67bf422a2b9a652c2e38cbb2f963fc2aeddb6151
Alex Cannizzo <cansal89@gmail.com>
2021-10-06 22:56:40 +0700

    Initial Commit

---
+++ City Sights/City_SightsApp.swift
@@ -0,0 +1,17 @@
+//
+//  City_SightsApp.swift
+//  City Sights
+//
+//  Created by Alex Cannizzo on 06/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct City_SightsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
